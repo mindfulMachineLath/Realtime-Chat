1b2372918dde132c035cda99c9996fd2096784b6
undersky0827 <william.dev.support@gmail.com>
2023-05-30 22:03:02 +0300

    refactor: change primary theme antd

--- src/app/antd.config.ts
+++ src/app/antd.config.ts
@@ -3,12 +3,15 @@ import { ThemeConfig } from 'antd';
 // Test color config, here we can setup theme config for Ant Design
 export const customTheme: ThemeConfig = {
   token: {
-    colorTextHeading: '#281E5B', // Dark blue
-    colorPrimary: '#281E5B',
-    colorText: '#281E5B',
-    // colorInfo: '#7156d9b8',
-    colorInfo: '#281E5B',
-    colorBgBase: '#ffffff',
-    colorTextSecondary: '#6F7A8F',
+    colorPrimaryBg: '#d3ebe5',
+    colorPrimaryBgHover: '#a0ded1',
+    colorPrimaryBorder: '#75d1c2',
+    colorPrimaryBorderHover: '#4fc4b5',
+    colorPrimaryHover: '#2cb8aa',
+    colorPrimary: '#0daba1',
+    colorPrimaryActive: '#048580',
+    colorPrimaryTextHover: '#2cb8aa',
+    colorPrimaryText: '#0daba1',
+    colorPrimaryTextActive: '#048580',
   },
 };
