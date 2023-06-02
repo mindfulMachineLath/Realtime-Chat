329fd9a5341b4b1ab02052c24c56db89356e9c5b
undersky0827 <william.dev.support@gmail.com>
2023-06-02 21:26:20 +0300

    feat: add form on home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react';
 import reactLogo from '../../assets/react.svg';
 import viteLogo from '/icon.svg';
 import styles from './Home.module.scss';
-import { Button, Input, Select, Form } from 'antd';
+import { Button, Input, Select, Form, Typography } from 'antd';
 
 export const Home: React.FC = () => {
-  const [count, setCount] = useState(0);
+  const [codeCountry, setCode] = useState('+7');
 
   const handleChange = (value: string) => {
     console.log(`selected ${value}`);
+  };
+
+  const handleSubmit = (event: Event) => {
+    console.log('on sumbit');
   };
 
   const prefixSelector = (
@@ -22,33 +26,59 @@ export const Home: React.FC = () => {
 
   return (
     <>
-      <div className={styles.main_box}>
-        <Select
-          defaultValue="lucy"
-          style={{ width: 120 }}
-          onChange={handleChange}
-          options={[
-            { value: 'jack', label: 'Jack' },
-            { value: 'lucy', label: 'Lucy' },
-            { value: 'Yiminghe', label: 'yiminghe' },
-            { value: 'disabled', label: 'Disabled', disabled: true },
-          ]}
-        />
+      <div className={styles.main_container}>
+        <div className={styles.main_wrapper}>
+          <div className={styles.home_logo}></div>
+          <Typography.Title>Chatty</Typography.Title>
 
-        <Form.Item
-          name="phone"
-          label="Phone Number"
-          rules={[
-            { required: true, message: 'Please input your phone number!' },
-          ]}
-        >
-          <Input
-            type="tel"
-            addonBefore={prefixSelector}
-            style={{ width: 200 }}
-            placeholder="‒‒‒ ‒‒‒ ‒‒‒‒"
-          />
-        </Form.Item>
+          <Typography.Text className={styles.note}>
+            Please confirm your country code and enter your phone number.
+          </Typography.Text>
+
+          <Form
+            name="login"
+            layout="vertical"
+            onFinish={handleSubmit}
+            className={styles.login_form}
+            initialValues={{ remember: true }}
+            labelWrap
+            labelCol={{ span: 10, offset: 0 }}
+            requiredMark={false}
+            size="large"
+          >
+            <Form.Item name="country" label="Country">
+              <Select
+                defaultValue="lucy"
+                style={{ width: '100%' }}
+                onChange={handleChange}
+                options={[
+                  { value: 'jack', label: 'Jack' },
+                  { value: 'lucy', label: 'Lucy' },
+                  { value: 'Yiminghe', label: 'yiminghe' },
+                  { value: 'disabled', label: 'Disabled', disabled: true },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item
+              name="phone"
+              label="You phone number"
+              rules={[
+                { required: true, message: 'Please input your phone number!' },
+              ]}
+            >
+              <Input
+                type="tel"
+                style={{ width: '100%' }}
+                placeholder="‒‒‒ ‒‒‒ ‒‒‒‒"
+                prefix={codeCountry}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button style={{ width: '100%' }}>next</Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     </>
   );
