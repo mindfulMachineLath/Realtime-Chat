882eb5cdd23b68a32ed705ee71dc62ff1a3cb3cb
undersky0827 <william.dev.support@gmail.com>
2023-07-04 19:02:34 +0300

    feat: add input for message component

--- src/pages/Main/ui/components/ChatPanel/ui/components/Input/Input.tsx
+++ src/pages/Main/ui/components/ChatPanel/ui/components/Input/Input.tsx
@@ -1,28 +1,33 @@
-import { Box, IconButton, InputAdornment, TextField } from '@mui/material';
+import React from 'react';
+import { Box, IconButton, InputBase } from '@mui/material';
 import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import SendIcon from '@mui/icons-material/Send';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 import s from './Input.module.scss';
 
 const Input: React.FC = () => {
   return (
     <Box className={s.send_box}>
-      <TextField
+      {/* TODO: прикрутить файлы */}
+      <IconButton aria-label="delete" onClick={() => console.log('FILE')}>
+        <AttachFileIcon color="primary" />
+      </IconButton>
+
+      <InputBase
         multiline={true}
         className={s.send_input}
-        id="input-message"
-        placeholder="message"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SentimentSatisfiedAltIcon />
-            </InputAdornment>
-          ),
-        }}
-        // variant="standard"
+        sx={{ ml: 1, flex: 1, color: '#0daba0' }}
+        placeholder="write a message"
+        inputProps={{ 'aria-label': 'write a message' }}
       />
 
+      {/* TODO: прикрутить эмоции */}
+      <IconButton aria-label="delete" onClick={() => console.log('ICON')}>
+        <SentimentSatisfiedAltIcon color="primary" />
+      </IconButton>
+
       <IconButton aria-label="delete">
-        <ArrowForwardIosIcon />
+        <SendIcon color="primary" />
       </IconButton>
     </Box>
   );
--- src/pages/Main/ui/components/ChatPanel/ui/components/Messages/Messages.tsx
+++ src/pages/Main/ui/components/ChatPanel/ui/components/Messages/Messages.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Message from './Message/Message';
 import s from './Messages.module.scss';
 
