4ea17ae471391d1bdd68bf9eadccacc68af31594
undersky0827 <william.dev.support@gmail.com>
2023-07-06 23:44:02 +0300

    fix: change style in form input mui tel

--- src/features/form/login/ui/components/Form/index.tsx
+++ src/features/form/login/ui/components/Form/index.tsx
@@ -4,6 +4,21 @@ import { useForm, Controller, FieldValues } from 'react-hook-form';
 import { MuiTelInput, matchIsValidTel } from 'mui-tel-input';
 import { LoadingButton } from 'shared/ui';
 import { MenuProps } from './style';
+import styled from '@emotion/styled';
+
+const MuiTelInputStyled = styled(MuiTelInput)`
+  & input {
+    color: #ffffffa8;
+  }
+
+  & label {
+    color: #ffffffa8;
+  }
+
+  & fieldset {
+    border-color: #ffffffa8;
+  }
+`;
 
 interface FormProps {
   onClick: (data: FormValue) => void;
@@ -29,7 +44,7 @@ const Form: React.FC<FormProps> = ({ onClick, loading }) => {
           control={control}
           rules={{ validate: matchIsValidTel }}
           render={({ field, fieldState }) => (
-            <MuiTelInput
+            <MuiTelInputStyled
               label={
                 fieldState.invalid ? 'Phone Number Invalid' : 'Phone Number'
               }
